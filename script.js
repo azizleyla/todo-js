83d6572ccdde62e939da68d34f162ca10af88458
Leyla Aziz <ezizovaleyla1@gmail.com>
2023-07-16 12:48:43 +0400

    refactor codes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const todoList = document.getElementById("todo-list");
 const hideBtn = document.getElementById("hide-btn");
 const showCompletedBtn = document.getElementById("completed");
 const allBtn = document.getElementById("all");
+const count = document.getElementById("count");
 
 //declare todos
 let todos = [];
@@ -19,8 +20,9 @@ const addTodo = (e) => {
     todoName: inputValue,
   };
   todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
-  updateTodos(todos);
+  setTodosToLS(todos);
+  updateUI();
+  updateCount()
 };
 
 //reset Fields
@@ -29,88 +31,97 @@ const resetState = () => {
   todoList.innerHTML = "";
 };
 
-//update UI
-const updateTodos = (todos) => {
-  resetState();
-  console.log("tt", todos);
-  todos.forEach((todo) => {
-    const { todoName, id, checked } = todo;
-    let element = `
-           <li class="todo__list-item" onclick="handleCheckTodo('${
-             todo.id
-           }')" data-id="${id}">
-        <div class="todo__list-checkbox">
-        <input type="radio" class="check"  ${
-          checked ? "checked" : ""
-        }  data-id="${id}">
+//create todo Element
+const createTodoElement = (todo) => {
+  const { id, todoName, checked } = todo;
+  return `
+    <li class="todo__list-item" onclick="handleCheckTodo('${id}')" data-id="${id}">
+      <div class="todo__list-checkbox">
+        <input type="radio" class="check" ${checked ? "checked" : ""} data-id="${id}">
         <span class="geekmark"></span>
-        </div>
-          <span class="todo__list-title ${
-            checked ? "active" : "false"
-          }" data-id="${id}">${todoName}</span>
-         
-            <button  onclick='handleDeleteTodo(event,"${
-              todo.id
-            }")' type="button" class="delete">
-            <img src="images/icon-cross.svg" class="delete" data-id="${
-              todo.id
-            }">
-            </button>
-       </li>
-        `;
+      </div>
+      <span class="todo__list-title ${checked ? "active" : "false"}" data-id="${id}">${todoName}</span>
+      <button onclick='handleDeleteTodo(event,"${id}")' type="button" class="delete">
+        <img src="images/icon-cross.svg" class="delete">
+      </button>
+    </li>
+  `;
+};
 
+
+//update UI
+const updateUI = () => {
+  resetState();
+  todos.forEach((todo) => {
+    const element = createTodoElement(todo)
     todoList.insertAdjacentHTML("afterbegin", element);
   });
 };
 const handleCheckTodo = (id) => {
   const selectedTodo = todos.find((item) => item.id === id);
   selectedTodo.checked = !selectedTodo.checked;
-
-  updateTodos(todos);
-  localStorage.setItem("todos", JSON.stringify(todos));
+  updateUI();
+  updateCount();
+  setTodosToLS();
 };
 
 //delete selected todo
 const handleDeleteTodo = (event, id) => {
   event.stopPropagation();
   todos = todos.filter((item) => item.id !== id);
-  updateTodos(todos);
-  localStorage.setItem("todos", JSON.stringify(todos));
+  updateUI();
+  updateCount();
+  setTodosToLS();
 };
-form.addEventListener("submit", addTodo);
 
 //get todos from Local Storage
-const getAllTodosFromLs = () => {
-  return JSON.parse(localStorage.getItem("todos")) || [];
-};
-//get initial Todos from LS
-window.addEventListener("DOMContentLoaded", function () {
+
+const filterAndShowTodos = (filterFn) => {
   const todosFromLs = getAllTodosFromLs();
-  updateTodos(todosFromLs);
-  todos = todosFromLs;
-});
-let isFlag = false;
+  const filteredTodos = todosFromLs.filter(filterFn);
+  todos = filteredTodos;
+  updateUI()
+}
 
 const hideCompletedTodos = () => {
-  const todosFromLs = getAllTodosFromLs();
-
-  let filteredTodos = todosFromLs.filter((item) => !item.checked);
-  updateTodos(filteredTodos);
-  todos = filteredTodos;
+  filterAndShowTodos(item => !item.checked)
 };
+
 
 const showCompletedTodos = () => {
   const todosFromLs = getAllTodosFromLs();
-  let filteredTodos = todosFromLs.filter((item) => item.checked);
-  updateTodos(filteredTodos);
-  todos = filteredTodos;
+  showCompletedBtn.classList.add("active");
+  allBtn.classList.remove("active");
+
+  filterAndShowTodos(item => item.checked)
 };
 const showAllTodos = () => {
   todos = getAllTodosFromLs();
-  updateTodos(todos);
+  updateUI();
+  showCompletedBtn.classList.remove("active");
+  allBtn.classList.add("active");
+};
+const updateCount = () => {
+  const undone = todos.filter((item) => !item.checked).length;
+  count.innerHTML = `${undone} items left`;
 };
 
+const getAllTodosFromLs = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+const setTodosToLS = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+form.addEventListener("submit", addTodo);
 allBtn.addEventListener("click", showAllTodos);
 hideBtn.addEventListener("click", hideCompletedTodos);
-
 showCompletedBtn.addEventListener("click", showCompletedTodos);
+
+
+//get initial Todos from LS
+window.addEventListener("DOMContentLoaded", function () {
+  todos = getAllTodosFromLs();
+  updateUI();
+  updateCount();
+});
